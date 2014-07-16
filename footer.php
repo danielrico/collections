25f7d8cde7b010309daf446c2666e6610e34508e
daniel.r <daniel.r@maps-system.com>
2014-07-16 16:32:05 +0200

    Added link to github

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div id="overlay"></div>
     </main>
     <footer class="wrapper">
-      <div id="about">A bunch of interesting articles collected by <a href="http://www.daniel-rico.com">Daniel Rico</a></div>
+      <div id="about">A bunch of interesting articles collected by <a href="http://www.daniel-rico.com">Daniel Rico</a>. Go ahead and <a href="https://github.com/danielrico/collections">fork this theme on github</a>.</div>
       <?php wp_footer(); ?>
     </footer>
 </body>
