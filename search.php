e1b8b0b6f35a8d0d6c7922c21e810726d19cc04a
danielrico <maildanielrico@gmail.com>
2015-04-07 16:46:50 +0200

    search results with direct link to source

--- search.php
+++ search.php
@@ -4,22 +4,33 @@
 
 <?php if(have_posts()) : ?>
   <?php while(have_posts()) : the_post(); ?>
+  
+  <?php $link = get_post_meta($post->ID, 'url_link', true); ?>
+  <?php $title = get_post_meta($post->ID, 'url_title', true); ?>
+  <?php $thumb_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
+  
   <div id="post-<?php the_ID(); ?>" class="post">
-      <div class="thumbnail">
-        <a href="">
-          <?php
+      <div class="thumbnail" style="background-image:url(
+       <?php
             if ( has_post_thumbnail() ) {
-              the_post_thumbnail();
+              echo $thumb_url; 
             }
             else {
-              echo '<img src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/default_thumbnail.png" alt="illustration" />';
+              echo get_bloginfo( 'stylesheet_directory' ) . '/img/default_thumbnail.png';
             }
-          ?>
-        </a>
+       ?>);">
+        <a target="read_article" href="<?php echo $link; ?>"></a>
       </div>
       <div class="text">
-        <h1><a href=""><?php the_title(); ?></a></h1>
+        <h1><a target="read_article" href="<?php echo $link; ?>"><?php the_title(); ?></a></h1>
         <?php the_content(); ?>
+         
+        <div class="curated_from">
+          <p><a href="<?php echo $link; ?>">
+            <?php echo $title; ?>
+          </a></p>
+        </div>
+                        
         <div class="post_category <?php $category = get_the_category(); echo $category[0]->slug; ?>"> 
           <?php the_category(', '); ?>
         </div>
@@ -30,11 +41,6 @@
     <div class="nav-next"><?php previous_posts_link( 'Newer posts' ); ?></div>
     <div class="nav-previous"><?php next_posts_link( 'Older posts' ); ?></div>
   </div>
-  <?php else: ?>
-  <div id="no_content">
-    <p>Sorry, no posts matched your criteria.</p>
-  </div>
-
 <?php endif; ?>
 
 <?php get_footer(); ?>
